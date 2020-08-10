d3c10b1bfb451ed5b3fdb58b4ae318cbd1e37888
katjavolk <katja.volk@live.com>
2020-08-10 18:08:50 +0200

    setup vue and bootstrap

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import navbar from '@/navbar/nav.vue'
+export default {
+  name: 'index',
+  components: {
+    navbar	
+  }
+}
+
+</script>

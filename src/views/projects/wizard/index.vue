62fb1916eff8b1da37dc4ebb8a842aed0af4d472
Marc Fischer <marc@marcfischer.org>
2020-09-29 22:13:57 +0200

    responsive media breakpoints for image grid, mobile layout

--- src/views/projects/wizard/index.vue
+++ src/views/projects/wizard/index.vue
@@ -4,24 +4,122 @@
       <titel headline="Clod, the weather wizard" />
       <kVideo :video="videoid" />
       <paragraph />
+      <!--
       <template>
-        <div style="margin-top:60px;">
+        <div style="margin-top: 60px">
           <div class="gallery_wizard">
-            <figure class="gallery_item_wizard_1" style="background-color:red;">
-              <img src="@/assets/img/wizard_1.jpg" alt="Gallery image 2" class="gallery_img_wizard" />
+            <figure class="gallery_item_wizard_1" style="background-color: red">
+              <img
+                src="@/assets/img/wizard_1.jpg"
+                alt="Gallery image 2"
+                class="gallery_img_wizard"
+              />
             </figure>
-            <figure class="gallery_item_wizard_2" style="background-color:green;">
-              <img src="@/assets/img/wizard_2.png" alt="Gallery image 3" class="gallery_img_wizard" />
+            <figure
+              class="gallery_item_wizard_2"
+              style="background-color: green"
+            >
+              <img
+                src="@/assets/img/wizard_2.png"
+                alt="Gallery image 3"
+                class="gallery_img_wizard"
+              />
             </figure>
-            <figure class="gallery_item_wizard_3" style="background-color:purple;">
-              <img src="@/assets/img/wizard_3.png" alt="Gallery image 4" class="gallery_img_wizard" />
+            <figure
+              class="gallery_item_wizard_3"
+              style="background-color: purple"
+            >
+              <img
+                src="@/assets/img/wizard_3.png"
+                alt="Gallery image 4"
+                class="gallery_img_wizard"
+              />
             </figure>
-            <figure class="gallery_item_wizard_4" style="background-color:blue;">
-              <img src="@/assets/img/wizard_4.jpg" alt="Gallery image 5" class="gallery_img_wizard" />
+            <figure
+              class="gallery_item_wizard_4"
+              style="background-color: blue"
+            >
+              <img
+                src="@/assets/img/wizard_4.jpg"
+                alt="Gallery image 5"
+                class="gallery_img_wizard"
+              />
             </figure>
           </div>
         </div>
-      </template>
+      </template> -->
+      <!-- marcis vorschlag für auto-responsive von bootstrap -->
+      <div class="d-none d-sm-block">
+        <b-media right-align vertical-align="center">
+          <template v-slot:aside>
+            <img src="@/assets/img/wizard_1.jpg" alt="Gallery Image 2" />
+          </template>
+        </b-media>
+        <b-media right-align vertical-align="center">
+          <template v-slot:aside>
+            <img
+              src="@/assets/img/wizard_2.png"
+              alt="Gallery Image 2"
+              style="width: 50%"
+            />
+
+            <img
+              src="@/assets/img/wizard_3.png"
+              alt="Gallery Image 2"
+              style="width: 50%"
+            />
+          </template>
+        </b-media>
+
+        <b-media right-align vertical-align="center">
+          <template v-slot:aside>
+            <img
+              src="@/assets/img/wizard_4.jpg"
+              alt="Gallery image 5"
+              style="width: 100%"
+            />
+          </template>
+        </b-media>
+      </div>
+      <div class="d-block d-sm-none">
+        <b-row>
+          <b-col>
+            <img
+              src="@/assets/img/wizard_1.jpg"
+              alt="Gallery Image 2"
+              width="100%"
+              style="margin-top: 1rem;"
+            />
+          </b-col>
+          <div class="w-100"></div>
+          <b-col>
+            <img
+              src="@/assets/img/wizard_2.png"
+              alt="Gallery Image 2"
+              width="100%"
+              style="margin-top: 1rem;"
+            />
+          </b-col>
+          <div class="w-100"></div>
+          <b-col>
+            <img
+              src="@/assets/img/wizard_3.png"
+              alt="Gallery Image 2"
+              width="100%"
+              style="margin-top: 1rem;"
+            />
+          </b-col>
+          <div class="w-100"></div>
+          <b-col>
+            <img
+              src="@/assets/img/wizard_4.jpg"
+              alt="Gallery image 5"
+              width="100%"
+              style="margin-top: 1rem;"
+            />
+          </b-col>
+        </b-row>
+      </div>
     </b-container>
   </div>
 </template>

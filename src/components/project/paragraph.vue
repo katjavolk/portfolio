036d33bfbbe6724587aecac4a195c9b430f9dd63
Marc Fischer <marc@marcfischer.org>
2021-03-07 18:09:18 +0100

    fixes #6

--- src/components/project/paragraph.vue
+++ src/components/project/paragraph.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <blockquote class="blockqoute description">
-      <p class="mb-0">
-        <slot v-html="paragraph"></slot>
-      </p>
-    </blockquote>
+    <b-row>
+      <b-col lg="8" md="6" sm="12">
+        <blockquote class="blockqoute description">
+          <p class="mb-0">
+            <slot v-html="paragraph"></slot>
+          </p>
+        </blockquote>
+      </b-col>
+    </b-row>
   </div>
 </template>
 

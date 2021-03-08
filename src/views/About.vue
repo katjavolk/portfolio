fe78c78f4d3c9012d2c66b7c24b38da7b14d35dd
Marc Fischer <marc@marcfischer.org>
2021-03-08 20:50:55 +0100

    #7: Change about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,8 +11,13 @@
         </b-col>
         <b-col lg="5">
           <p class="context">
-            Hey, ich bin Katja. Designerin aus Kaiserslautern. Zur Zeit lebe,
-            arbeite und studiere ich in Würzburg.
+            Hey!
+            <br /> Ich bin Katja, Kommunikations- und Grafikdesignerin. Zur Zeit
+            lebe und arbeite ich in Würzburg.<br /> Mein Schwerpunkt liegt im
+            Digitalen und Interaktivem Design. Doch zu meinem Portfolio gehören
+            auch Motion und Grafik. Melde dich gerne bei mir, wenn du Interesse
+            an einer Zusammenarbeit hast oder mehr über eins meiner Projekte
+            wissen möchtest. <br /><br />Mail: <a class="kontakt-email-context" href="mailto:kontakt@katjavolk.de">kontakt@katjavolk.de</a>
           </p>
         </b-col>
         <b-col lg="2" class="d-none d-lg-block">&nbsp;</b-col>
@@ -25,6 +30,16 @@
 <style>
 .context {
   font-size: 1.5em;
+}
+
+.kontakt-email-context {
+  font-size: inherit;
+  font-family: Syne-Bold;
+  color: #000;
+  
+}
+.kontakt-email-context:hover {
+  color: #000;
 }
 </style>
 
